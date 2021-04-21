de7f93d4a29ffd17409d1ca57bb38f6f8b2334fe
nattigy <nathnaelakale@gmail.com>
2021-04-21 05:53:45 +0300

    add to category page improved

--- src/pages/admin/add_category.js
+++ src/pages/admin/add_category.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { gql, useMutation } from "@apollo/client";
-import { ContactSupportOutlined } from "@material-ui/icons";
 
 const ADD_CATEGORY = gql`
   mutation($record: CreateOneCategoryInput!) {
@@ -14,14 +13,40 @@ const ADD_CATEGORY = gql`
   }
 `;
 
+const ADD_TO_BUSINESSES_LIST = gql`
+  mutation($autocompleteTerm: String) {
+    businessListCreateOne(record: { autocompleteTerm: $autocompleteTerm }) {
+      record {
+        autocompleteTerm
+      }
+    }
+  }
+`;
+
 const AddCategory = () => {
   const [cat_name, setCatName] = useState("");
   const [cat_parent, setCatParent] = useState("");
 
-  const [addCategory, { loading, data, error }] = useMutation(ADD_CATEGORY);
+  const [
+    addCategory,
+    {
+      loading: addingToCategory,
+      data: addedToCategory,
+      error: addingToCategoryError,
+    },
+  ] = useMutation(ADD_CATEGORY);
+
+  const [
+    addBusinessList,
+    {
+      loading: addingToBusinessesList,
+      data: addedToBusinessesList,
+      error: addingToBusinessesListError,
+    },
+  ] = useMutation(ADD_TO_BUSINESSES_LIST);
 
   return (
-    <div>
+    <div className="container-md text-center py-5">
       <h1>Add Category</h1>
       <form
         onSubmit={(e) => {
@@ -33,33 +58,68 @@ const AddCategory = () => {
                 parent: cat_parent,
                 autocompleteTerm: cat_name + " " + cat_parent,
                 categoryIndex: [
-                  ...cat_name.split(" "),
-                  ...cat_parent.split(" "),
+                  ...cat_name.toLowerCase().split(" "),
+                  ...cat_parent.toLowerCase().split(" "),
                 ],
               },
             },
           });
+
+          addBusinessList({
+            variables: {
+              autocompleteTerm: cat_name.toLowerCase(),
+            },
+          });
         }}
       >
-        <input
-          placeholder="Category Name"
-          type="text"
-          value={cat_name}
-          onChange={(v) => setCatName(v.target.value)}
-        />
-        <br />
-        <input
-          placeholder="Category Parent"
-          type="text"
-          value={cat_parent}
-          onChange={(v) => setCatParent(v.target.value)}
-        />
-        <br />
-        <input type="submit" />
+        <div className="row py-5">
+          <div className="col-12 col-md-6 my-2">
+            <input
+              placeholder="Category Name"
+              className="w-100"
+              type="text"
+              value={cat_name}
+              onChange={(v) => setCatName(v.target.value)}
+            />
+          </div>
+          <div className="col-12 col-md-6 my-2">
+            <input
+              placeholder="Category Parent"
+              className="w-100"
+              type="text"
+              value={cat_parent}
+              onChange={(v) => setCatParent(v.target.value)}
+            />
+          </div>
+        </div>
+        <div className="mb-5">
+          <input type="submit" />
+          <div>
+            {addingToCategory && "Loading..."}
+            {addedToCategory && "Done"}
+            {addingToCategoryError && addingToCategoryError.message}
+          </div>
+        </div>
+        <div className="">
+          <button
+            onClick={() => {
+              addBusinessList({
+                variables: {
+                  autocompleteTerm: cat_parent.toLowerCase(),
+                },
+              });
+            }}
+            type="button"
+          >
+            Add parent to search index list
+          </button>
+          <div>
+            {addingToBusinessesList && "Loading..."}
+            {addedToBusinessesList && "Done"}
+            {addingToBusinessesListError && addingToBusinessesListError.message}
+          </div>
+        </div>
       </form>
-      {loading && "Loading..."}
-      {data && "Done"}
-      {error && error.message}
     </div>
   );
 };
